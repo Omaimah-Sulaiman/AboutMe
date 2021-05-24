d29b80d2467d3080b6572416c506cb6a8f25b1e3
omaimah-sulaiman <omaymahshakhatrah@gmail.com>
2021-05-24 14:45:13 +0300

    modify app.js add equstion

---
+++ js/app.js
@@ -0,0 +1,41 @@
+let userN =prompt('what is your name ?')
+alert('please answer me yess or no ')
+let education = prompt('Are you educative ?')
+if (!(education.toLowerCase() === 'yes'||education.toLowerCase() ==='no'||education.toLowerCase() ==='y'||education.toLowerCase() ==='n')){
+    alert('please answer me yes or no ')
+    education.toLowerCase = prompt('Are you educative ?')
+
+}
+
+console.log('education')
+
+let sport = prompt('Are you sporting  ?')
+if (!(sport.toLowerCase() === 'yes'||sport.toLowerCase() ==='no'||sport.toLowerCase() ==='y'||sport.toLowerCase() ==='n')){
+    alert('please answer me yes or no ')
+    sport = prompt('Are you sporting  ?')
+    console.log('sport')
+
+}
+
+let smoke =prompt ('Are you smoking ?')
+if (!(smoke.toLowerCase === 'yes'||smoke.toLowerCase ==='no'||smoke.toLowerCase() ==='y'||smoke.toLowerCase() ==='n')){
+    alert('please answer me yes or no ')
+    smoke =prompt ('Are you smoking ?')
+
+}
+
+let health = prompt('Do you suffer from health problems ?')
+console.log('health')
+if (!(health.toLowerCase() === 'yes'||health.toLowerCase() ==='no'||health.toLowerCase() ==='y'||health.toLowerCase() ==='n')){
+    alert('please answer me yes or no ')
+    health = prompt('Do you suffer from health problems ?')
+
+}
+let food = prompt('Do you eat helthy food ?')
+if (!(food.toLowerCase() === 'yes'||food.toLowerCase() ==='no'||food.toLowerCase() ==='y'||food.toLowerCase() ==='n')){
+    alert('please answer me yes or no ')
+    food = prompt('Do you eat helthy food ?')
+
+}
+
+
