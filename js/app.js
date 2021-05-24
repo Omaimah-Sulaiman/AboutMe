1b57b45259b81467d0480380e0bb72bfe0b6fffa
omaimah-sulaiman <omaymahshakhatrah@gmail.com>
2021-05-24 15:14:52 +0300

    add style to page

--- js/app.js
+++ js/app.js
@@ -1,44 +1,44 @@
-// 'use strict'
-// let userN =prompt('what is your name ?')
-// alert('welcome 🥰'+userN)
-// alert('please answer me yess or no ')
-// let education = prompt('Are you educative ?')
-// if (!(education.toLowerCase() === 'yes'||education.toLowerCase() ==='no'||education.toLowerCase() ==='y'||education.toLowerCase() ==='n')){
-//     alert('please answer me yes or no 🥺 ')
-//     education.toLowerCase = prompt('Are you educative ?')
+'use strict'
+let userN =prompt('what is your name ?')
+alert('welcome 🥰'+userN)
+alert('please answer me yess or no ')
+let education = prompt('Are you educative ?')
+if (!(education.toLowerCase() === 'yes'||education.toLowerCase() ==='no'||education.toLowerCase() ==='y'||education.toLowerCase() ==='n')){
+    alert('please answer me yes or no 🥺 ')
+    education.toLowerCase = prompt('Are you educative ?')
 
-// }
+}
 
-// console.log('education')
+console.log('education')
 
-// let sport = prompt('Are you sporting  ?')
-// if (!(sport.toLowerCase() === 'yes'||sport.toLowerCase() ==='no'||sport.toLowerCase() ==='y'||sport.toLowerCase() ==='n')){
-//     alert('please answer me yes or no 🥺')
-//     sport = prompt('Are you sporting  ?')
-//     console.log('sport')
+let sport = prompt('Are you sporting  ?')
+if (!(sport.toLowerCase() === 'yes'||sport.toLowerCase() ==='no'||sport.toLowerCase() ==='y'||sport.toLowerCase() ==='n')){
+    alert('please answer me yes or no 🥺')
+    sport = prompt('Are you sporting  ?')
+    console.log('sport')
 
-// }
+}
 
-// let smoke =prompt ('Are you smoking ?')
-// if (!(smoke.toLowerCase === 'yes'||smoke.toLowerCase ==='no'||smoke.toLowerCase() ==='y'||smoke.toLowerCase() ==='n')){
-//     alert('please answer me yes or no 🥺 ')
-//     smoke =prompt ('Are you smoking ?')
+let smoke =prompt ('Are you smoking ?')
+if (!(smoke.toLowerCase === 'yes'||smoke.toLowerCase ==='no'||smoke.toLowerCase() ==='y'||smoke.toLowerCase() ==='n')){
+    alert('please answer me yes or no 🥺 ')
+    smoke =prompt ('Are you smoking ?')
 
-// }
+}
 
-// let health = prompt('Do you suffer from health problems ?')
-// console.log('health')
-// if (!(health.toLowerCase() === 'yes'||health.toLowerCase() ==='no'||health.toLowerCase() ==='y'||health.toLowerCase() ==='n')){
-//     alert('please answer me yes or no 🥺 ')
-//     health = prompt('Do you suffer from health problems ?')
+let health = prompt('Do you suffer from health problems ?')
+console.log('health')
+if (!(health.toLowerCase() === 'yes'||health.toLowerCase() ==='no'||health.toLowerCase() ==='y'||health.toLowerCase() ==='n')){
+    alert('please answer me yes or no 🥺 ')
+    health = prompt('Do you suffer from health problems ?')
 
-// }
-// let food = prompt('Do you eat helthy food ?')
-// if (!(food.toLowerCase() === 'yes'||food.toLowerCase() ==='no'||food.toLowerCase() ==='y'||food.toLowerCase() ==='n')){
-//     alert('please answer me yes or no 🥺 ')
-//     food = prompt('Do you eat helthy food ?')
+}
+let food = prompt('Do you eat helthy food ?')
+if (!(food.toLowerCase() === 'yes'||food.toLowerCase() ==='no'||food.toLowerCase() ==='y'||food.toLowerCase() ==='n')){
+    alert('please answer me yes or no 🥺 ')
+    food = prompt('Do you eat helthy food ?')
 
-// }
-// alert('you made grate job'+userN +'❤️')
+}
+alert('you made grate job'+userN +'❤️')
 
 
